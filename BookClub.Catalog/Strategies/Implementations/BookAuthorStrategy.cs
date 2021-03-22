3a3b4ecf857b85aa6b5928118e1d01edb9113891
Neil Earlam <neil.earlam2019@outlook.com>
2021-03-22 00:08:33 +0000

    Added new strategies to deal with the new model relationships

---
+++ BookClub.Catalog/Strategies/Implementations/BookAuthorStrategy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BookClub.Catalog.Models;
+
+namespace BookClub.Catalog.Strategies.Implementations
+{
+    public class BookAuthorStrategy : IBookAuthorStrategy
+    {
+        public bool CanAddAuthor
+        (
+            IEnumerable<BookAuthor> bookAuthors,
+            Author author
+        )
+        {
+            return !bookAuthors
+                .Where(x => x.Author.NormalizedName == author.NormalizedName)
+                .Any();
+        }
+
+        public bool CanRemoveAuthor
+        (
+            IEnumerable<BookAuthor> bookAuthors,
+            int id
+        )
+        {
+            return AuthorCountAboveOne(bookAuthors)
+                && AuthorIdContainedWithinCollection(bookAuthors, id);
+        }
+
+        private static bool AuthorCountAboveOne(IEnumerable<BookAuthor> bookAuthors)
+        {
+            return bookAuthors.Count() > 1;
+        }
+
+        private static bool AuthorIdContainedWithinCollection
+        (
+            IEnumerable<BookAuthor> bookAuthors,
+            int id
+        )
+        {
+            return bookAuthors.Where(x => x.AuthorId == id).Any();
+        }
+    }
+}
---
+++ BookClub.Catalog/Strategies/Implementations/PublisherStrategy.cs
@@ -0,0 +1,16 @@
+﻿using BookClub.Catalog.Models;
+
+namespace BookClub.Catalog.Strategies.Implementations
+{
+    public class PublisherStrategy : IPublisherStrategy
+    {
+        public bool CanChangePublisher
+        (
+            Publisher currentPublisher,
+            Publisher newPublisher
+        )
+        {
+            return currentPublisher.NormalizedName != newPublisher.NormalizedName;
+        }
+    }
+}
